657c9bf2c50bc2bf92d2c044ed61afb121d44e46
nawazyarkhan <nawazyarkhan@gmail.com>
2022-01-02 21:58:04 +0000

    python-api-challenge homework part1

---
+++ VacationPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -0,0 +1,47 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "76721079",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "api_key_nyt=\"DTlmi68Yr0u9NF47H77TEp0NpGMoICt9\"\n",
+    "\n",
+    "api_key_weather=\"49c1e184f4441fdd8d56fe0e367da3fa\"\n",
+    "\n",
+    "api_key_google=\"AIzaSyD_yzNEde6lqnx9DZ2WY1bNnERkgL5wT4s\"\n",
+    "\n",
+    "api_key_census=\"27c72963e70e31dafba59a530929c6b3b5f70b75\"\n",
+    "\n",
+    "api_key_omdb=\"150e617d\"\n",
+    "\n",
+    "api_key_moonstock=\"API_KEY736SWRGKPC4HN46XKIRVAMTKATE6BSMC\"\n",
+    "\n",
+    "gkey=\"AIzaSyD_yzNEde6lqnx9DZ2WY1bNnERkgL5wT4s\""
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.6.10"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
